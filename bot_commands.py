06e9d87baed3c832aff0a6995caf9e7a522624b5
AndreyKorovin <martovsky910@gmail.com>
2022-08-23 19:57:59 +0300

    hw9

--- bot_commands.py
+++ bot_commands.py
@@ -2,23 +2,37 @@ from time import time
 from pytube import YouTube
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
-import datetime
 from spy import *
 
 
-# def add(name):
-#     name - name.get()
-#     DLlowpres_video()
-
-async def DLlowpres_video(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    # log(Update, context)
+async def DLlowest_video(update: Update, context: ContextTypes.DEFAULT_TYPE):
     msg = update.message.text
     url = msg
     print(url)
     getVideo = YouTube(url)
-    getVideo.streams.filter(progressive=True, file_extension='mp4').get_lowest_resolution().download('D:\GeekBrains\Python')
+    getVideo.streams.get_lowest_resolution().download('D:\GeekBrains\Python') #.get_lowest_resolution().download('D:\GeekBrains\Python')
     print('Видео в низком качестве успешно скачалось')
     await update.message.reply_text('Видео в низком качестве успешно скачалось')
+
+
+async def DLhighest_video(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    msg = update.message.text
+    url = msg
+    print(url)
+    getVideo = YouTube(url)
+    getVideo.streams.get_highest_resolution().download('D:\GeekBrains\Python') #.filter(progressive=True, file_extension='mp4')
+    print('Видео в высоком качестве успешно скачалось')
+    await update.message.reply_text('Видео в высоком качестве успешно скачалось')
+
+
+async def DL_audio(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    msg = update.message.text
+    url = msg
+    print(url)
+    getAudio = YouTube(url)
+    getAudio.streams.get_audio_only().download(output_path='D:\GeekBrains\Python', filename='audio.mp3')
+    print('Аудио успешно скачалось')
+    await update.message.reply_text('Аудио успешно скачалось')
 
 
 async def hi_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -26,12 +40,7 @@ async def hi_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     log(Update,context)
     await update.message.reply_text(f'hi {update.effective_user.first_name}') 
 
-def time_command(update: Update, context: ContextTypes.DEFAULT_TYPE): 
+
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     log(Update,context)
-    update.message.reply_text(f'time {datetime.datetime.now().time()}')
-
-def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    log(Update,context)
-    update.message.reply_text(f'/hi\n/time\n/help')
-
-
+    await update.message.reply_text(f'/hi\n/time\n/dl_low\n/dl_hight\n/dl_audio\n/help')
--- main.py
+++ main.py
@@ -2,12 +2,13 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 from bot_commands import *
 
-app = ApplicationBuilder().token("5780455225:AAFPoFkl0R9mjm91VweO7cZziVtL3QcUcZE").build()
+app = ApplicationBuilder().token("token").build()
 
-app.add_handler(CommandHandler("hi", DLlowpres_video))
-# app.add_handler(CommandHandler("time", time_command))
-# app.add_handler(CommandHandler("help", help_command))
-# app.add_handler(CommandHandler("sum", sum_command))
+app.add_handler(CommandHandler("dl_low", DLlowest_video))
+app.add_handler(CommandHandler("dl_hight", DLhighest_video))
+app.add_handler(CommandHandler("dl_audio", DL_audio))
+app.add_handler(CommandHandler("hi", hi_command))
+app.add_handler(CommandHandler("help", help_command))
 
 print('server start')
 
